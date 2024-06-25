4fbb8a85d71c5ac2ef9bcd7ba1e011fa9163346e
V <77253008+vonque@users.noreply.github.com>
2024-06-24 23:46:22 -0400

    line indent : 2:48 min into task managment tutorial python

--- calendar.py
+++ calendar.py
@@ -1,1 +1,27 @@
+""" Calendar task manager """
+from flet import flet,Page, Column, Row, Container, padding
 
+def main(page:Page):
+    
+
+    #
+    _main = Container(
+        width = 290,
+        height = 590,
+        bgcolor = "black",
+        padding = 8,
+    )
+
+
+
+    page.add(_main)
+    page.horizontal_alignment = 'center'
+    page.vertical_alignment = 'center'
+    page.update()
+
+
+
+
+
+if __name__ == "__main__": 
+    flet.app(target=main)
